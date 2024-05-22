965d691e985c9e1859283c874ccd2d4f55ff101b
cuberry <joon8454@gmail.com>
2024-05-23 00:53:17 +0900

    scripts run on background

--- reference.sh
+++ reference.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 read $1
-firefox -new-tab "https://www.acmicpc.net/problem/$1" 2>/dev/null
+firefox -new-tab "https://www.acmicpc.net/problem/$1" &2>/dev/null &
 #xdg-open "https://www.acmicpc.net/problem/$no"
 exit 0
--- wrtn.sh
+++ wrtn.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-firefox -new-tab "https://wrtn.ai/" 2>/dev/null
+firefox -new-tab "https://wrtn.ai/" &2>/dev/null &
 #xdg-open "https://www.acmicpc.net/problem/$no"
 exit 0
+
